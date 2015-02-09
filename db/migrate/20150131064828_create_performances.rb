c7c1f726cd2339a679b9d7ea43d8196cd5d3b20d
becky <rebecca.russoniello@gmail.com>
2015-02-08 22:47:53 -0600

    add net time, gun time, and average pace to performance.

--- db/migrate/20150131064828_create_performances.rb
+++ db/migrate/20150131064828_create_performances.rb
@@ -1,13 +1,14 @@
 class CreatePerformances < ActiveRecord::Migration
-  def change
+  def up
     create_table :performances do |t|
       t.references :runner, index: true
+      t.references :race, index: true
       t.string :bib_number
       t.integer :age
       t.string :location
-      t.time :net_time
-      t.time :gun_time
-      t.time :average_pace
+      t.integer :net_time
+      t.integer :gun_time
+      t.integer :average_pace
       t.references :division, index: true
       t.references :sex, index: true
       t.integer :division_place
@@ -18,5 +19,9 @@ class CreatePerformances < ActiveRecord::Migration
     add_foreign_key :performances, :runners
     add_foreign_key :performances, :divisions
     add_foreign_key :performances, :sexes
+  end
+
+  def down
+    drop_table :performances
   end
 end
