cd47529f8253f9de438275a4890fe3a77becd490
becky <rebecca.russoniello@gmail.com>
2015-02-13 17:49:22 -0600

    refactor scrapers

--- lib/scrapers/scraper.rb
+++ lib/scrapers/scraper.rb
@@ -8,11 +8,65 @@ module Scraper
 	SECONDS_IN_AN_HOUR = 3600
 	SECONDS_IN_A_MINUTE = 60
 
-	attr_accessor :race, :url, :rows, :row_index, :page_number, :total_page_count
+	attr_accessor :race, :url, :rows, :row_index, :page_number, :total_page_count, :current_sex
 
 	def initialize(race)
 		@race = race
 		@url = race.results_url
+		@page_number = 1
+		@row_index = 1
+	end
+
+	def scrape(need_sleep = false)
+		navigate_to_first_page
+		sleep(2) if need_sleep
+		set_up_pagination
+
+		while page_number <= total_page_count
+			save_results_from_page
+			go_to_next_page
+			sleep(2) if need_sleep
+		end
+	end
+
+	def save_results_from_page
+		self.row_index = first_row_index
+		
+		begin
+			find_data_rows
+			rows.each do |row|
+				save_performance(row)
+				self.row_index = row_index + 1
+			end
+		rescue Capybara::Poltergeist::ObsoleteNode => e
+			puts e.message
+			retry
+		end
+	end
+
+	def find_data_rows
+		self.rows = all(row_selector)[row_index..-1]
+	end
+
+	def save_performance(result)
+		cells = result.all('td')
+		return unless cells.any?
+
+		runner_name = cells[name_cell_index].text.titleize
+		bib_number = cells[bib_number_cell_index].text
+
+		runner = Runner.find_or_create_by(name: runner_name)
+		performance = race.performances.find_or_initialize_by(runner: runner, bib_number: bib_number)
+
+		record_performance_stats(performance, cells)
+		performance.save
+	end
+
+	def go_to_next_page
+		unless page_number == total_page_count
+			navigate_to_next_page
+		end
+		self.page_number += 1
 	end
 
 	def seconds_elapsed(time_string)
--- lib/scrapers/track_shack.rb
+++ lib/scrapers/track_shack.rb
@@ -1,19 +1,9 @@
 class TrackShack
 	include Scraper
 
-	attr_accessor :divisions, :current_sex
+	attr_accessor :divisions
 
-	def scrape
-		navigate_to_results
-		set_up_pagination
-
-		while page_number <= total_page_count
-			save_results_from_page
-			go_to_next_page
-		end
-	end
-
-	def navigate_to_results
+	def navigate_to_first_page
 		visit url
 		click_button("Search")
 	end
@@ -24,49 +14,37 @@ class TrackShack
 		# need to keep wheelchair separate.  will add back once i flesh out logic re: divisions
 		self.divisions = divisions.reject{ |div| div.match(/wheelchair/i) }
 		self.total_page_count = divisions.size
-		self.page_number = 1
 	end
 
-	def save_results_from_page
-		self.row_index = 0
-		self.rows = all("tr[class$='_row']")
-		
-		begin
-			rows.each do |row|
-				save_performance(row)
-				self.row_index = row_index + 1
-			end
-		rescue Capybara::Poltergeist::ObsoleteNode => e
-			puts e.message
-			self.rows = all("tr[class$='_row']")
-			self.row_index = 0
-			retry
-		end
+	def row_selector
+		"tr[class$='_row']"
 	end
 
-	def go_to_next_page
-		unless page_number == total_page_count
-			next_division = divisions[page_number]
-			select(next_division, from: "select1")
-			click_button("Search")
-			self.current_sex = next_division.match(/women/i) ? SEXES['F'] : SEXES['M']
-		end
-		self.page_number += 1
+	def first_row_index
+		0
 	end
 
-	def save_performance(result)
-		cells = result.all('td')
-		return unless cells.any?
+	def navigate_to_next_page
+		next_division = divisions[page_number]
+		select(next_division, from: "select1")
+		click_button("Search")
+		self.current_sex = next_division.match(/women/i) ? SEXES['F'] : SEXES['M']
+	end
 
-		runner = Runner.find_or_create_by(name: cells[1].text.titleize)
-		performance = race.performances.find_or_initialize_by(runner: runner, bib_number: cells[2].text)
+	def name_cell_index
+		1
+	end
 
+	def bib_number_cell_index
+		2
+	end
+
+	def record_performance_stats(performance, cells)
 		performance.age = cells[3].text
 		performance.sex = current_sex
 		performance.location = cells[11].text
 		performance.net_time = seconds_elapsed(cells[10].text)
 		performance.gun_time = seconds_elapsed(cells[9].text)
 		performance.average_pace = performance.net_time.to_f / RACE_LENGTH_IN_MILES
-		performance.save
 	end
 end
