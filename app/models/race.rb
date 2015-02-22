24eb101d934feacaf5ffddfbc3c9a2332ca1cd9f
becky <rebecca.russoniello@gmail.com>
2015-02-22 14:28:23 -0600

    validate year by inclusion in range

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,7 +4,7 @@ class Race < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: { scope: :year } 
   validates :date, :location, presence: true
-  validates :year, presence: true, inclusion: { in: [1990..Date.today.year]}
+  validates :year, presence: true, inclusion: { in: (1990..Date.today.year)}
   validates :results_provider, presence: true, inclusion: { in: RESULTS_PROVIDERS }
   validates :website, presence: true, format: { with: URI.regexp }
   validate :date_and_year_match
