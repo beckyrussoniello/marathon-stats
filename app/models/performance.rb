041c9c9c3d71fded1465edefd0faaf11466c81cc
becky <rebecca.russoniello@gmail.com>
2015-02-24 18:15:14 -0600

    update Performance's uniqueness validation for runner attribute

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -4,7 +4,7 @@ class Performance < ActiveRecord::Base
   belongs_to :division
   belongs_to :sex
 
-  validates :runner, presence: true, uniqueness: {scope: :race_id}
+  validates :runner, presence: true, uniqueness: {scope: :event_id}
   validates :event, presence: true
   validates :bib_number, presence: true
   validates :age, :gun_time, :net_time, :average_pace, numericality: true, allow_nil: true
